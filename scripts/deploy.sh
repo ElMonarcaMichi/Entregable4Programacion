adaa76666d826b89b6b587ebecb611a516121188
Santiago Coll <scoll@correo.um.edu.uy>
2025-11-17 17:32:06 -0300

    deploy para linux

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Iniciando deploy en Linux/Mac..."
+
+APP_PATH="/var/jenkins_home/workspace/Entregable4/deploy/app.jar"
+LOG_PATH="/var/jenkins_home/workspace/Entregable4/app.log"
+
+echo "Matando procesos viejos..."
+pkill -f app.jar || true
+
+echo "Levantando app..."
+nohup java -jar "$APP_PATH" > "$LOG_PATH" 2>&1 &
+
+echo "Deploy terminado"
